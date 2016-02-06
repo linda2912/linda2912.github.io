1fc809113a4b7d201c2b8b12b10f4f7b6c5cf383
Linda <lindavandijkdesign@gmail.com>
2016-02-06 13:01:25 +0100

    update

---
+++ webAppFromScratch/opdracht 5/static/app.js
@@ -0,0 +1,55 @@
+
+(function() {  // use IIFE to avoid global vars
+	
+	"use strict" // with strict mode, you can not, for example, use undeclared variables
+
+	var app = { // literal object
+		init: function() { 
+			routes.init(); // run the function app.routes.init.
+		}
+	};
+
+
+	var routes = { //routes function
+
+	    oldUrl: "",
+	    newUrl: "",
+
+		init: function () { //method
+			sections.toggle(null, 'home'); 
+			window.addEventListener('hashchange', function (event) { //if the hash changes run sections.toggle
+
+				this.newUrl = event.newURL.split('#')[1];
+				this.oldUrl = event.oldURL.split('#')[1];
+
+				sections.toggle(this.oldUrl, this.newUrl);
+
+			}, false);
+		}
+	};
+
+	var sections = { // toggle between the sections
+		toggle: function (oldRoute, newRoute) {
+			var oldRouteElement,
+			    newRouteElement = false;
+
+
+			// desired section is shown and will be hidden from all the other sections
+			if (oldRoute) {
+
+				oldRouteElement = document.getElementById(oldRoute);
+				oldRouteElement.classList.add('invisible');
+			}
+
+			newRouteElement = document.getElementById(newRoute);
+			newRouteElement.classList.remove('invisible');
+
+		}
+	}
+
+	app.init(); //run the app
+
+}());
+
+// refresh werkend maken is er al een hash?
+// issues uitzoeken
