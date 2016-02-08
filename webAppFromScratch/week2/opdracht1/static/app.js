4d3e89110b31ba956c9c90c4190d97ee888e5a98
Linda <lindavandijkdesign@gmail.com>
2016-02-08 16:18:53 +0100

    update

--- webAppFromScratch/week2/opdracht1/static/app.js
+++ webAppFromScratch/week2/opdracht1/static/app.js
@@ -1,81 +1,81 @@
 
-(function() {  // use IIFE to avoid global vars
+// (function() {  // use IIFE to avoid global vars
 	
-	"use strict" // with strict mode, you can not, for example, use undeclared variables
+// 	"use strict" // with strict mode, you can not, for example, use undeclared variables
 
-	var main = document.querySelector('main');
+// 	var main = document.querySelector('main');
 
-	var app = { // literal object
-		init: function() { 
-			this.routes();
-			// routes.init(); // run the function app.routes.init.
-		},
-
-
-		routes: function () {
-			routie({
-			    'home': function() {
-			    	sections.toggle();
-			    },
-			    'frontEndBestPractices': function() {
-			    	sections.toggle();
-			    	function loadDoc () { //key jB5D6SNV
-			    		console.log("running loaddoc");
-			    		var xhttp = new XMLHttpRequest();
-			    		console.log(xhttp.open("GET", "https://www.rijksmuseum.nl/api/pages/nl/ontdek-de-collectie/overzicht/rembrandt-harmensz-van-rijn?key=jB5D6SNV&format=jsonp", true));
-			    		console.log(xhttp.send());
-			    		console.log(xhttp.responseText);
-
-			    		xhttp.onreadystatechange = function() {
-						  if (xhttp.readyState == 4 && xhttp.status == 200) {
-						    console.log(xhttp.responseText);
-						  }
-						};
-			    	}
-			    	loadDoc();
-
-			    }
-			});
-		}
-	};
-
-	// This ideas comes from Dylan
-	// var routes = { //routes function
-	// 	init: function() {
-	// 		window.addEventListener('hashchange', sections.toggle); //when changing the url hash, starts the sections toggle function
-	// 		window.addEventListener('load', sections.toggle); //
-	// 	}
-
-	// };
-
-	var sections = { // toggle between the sections
-		toggle: function() {
-			var hash = window.location.hash; //get the hash on the current url after click and save in a varibale
-
-			if (hash) { 
-				var getHash = document.querySelector(hash); //get the template from the html that matched
-				if (getHash) { //if there is a hash, put the section in the html
-					main.innerHTML = getHash.innerHTML; 
-				} else {
-					main.innerHTML = document.querySelector('#error').innerHTML; //give the error page
-				}
-			} else {
-				window.location.hash = '#home'; // if there is no hash, give the home page
-			}
-		}
-	};
-
-	var hello = {
-		greeting: "Ph'nglui mglw'nafh Cthulhu R'lyeh wgah'nagl fhtagn",
-		translation: "In his house at R'lyeh, dead Cthulhu waits dreaming."
-	};
-
-	Transparency.render(document.getElementById('template'), hello);
+// 	var app = { // literal object
+// 		init: function() { 
+// 			this.routes();
+// 			// routes.init(); // run the function app.routes.init.
+// 		},
 
 
-	app.init(); //run the app
+// 		routes: function () {
+// 			routie({
+// 			    'home': function() {
+// 			    	sections.toggle();
+// 			    },
+// 			    'frontEndBestPractices': function() {
+// 			    	sections.toggle();
+// 			    	function loadDoc () { //key jB5D6SNV
+// 			    		console.log("running loaddoc");
+// 			    		var xhttp = new XMLHttpRequest();
+// 			    		console.log(xhttp.open("GET", "https://www.rijksmuseum.nl/api/pages/nl/ontdek-de-collectie/overzicht/rembrandt-harmensz-van-rijn?key=jB5D6SNV&format=jsonp", true));
+// 			    		console.log(xhttp.send());
+// 			    		console.log(xhttp.responseText);
 
-}());
+// 			    		xhttp.onreadystatechange = function() {
+// 						  if (xhttp.readyState == 4 && xhttp.status == 200) {
+// 						    console.log(xhttp.responseText);
+// 						  }
+// 						};
+// 			    	}
+// 			    	loadDoc();
+
+// 			    }
+// 			});
+// 		}
+// 	};
+
+// 	// This ideas comes from Dylan
+// 	// var routes = { //routes function
+// 	// 	init: function() {
+// 	// 		window.addEventListener('hashchange', sections.toggle); //when changing the url hash, starts the sections toggle function
+// 	// 		window.addEventListener('load', sections.toggle); //
+// 	// 	}
+
+// 	// };
+
+// 	var sections = { // toggle between the sections
+// 		toggle: function() {
+// 			var hash = window.location.hash; //get the hash on the current url after click and save in a varibale
+
+// 			if (hash) { 
+// 				var getHash = document.querySelector(hash); //get the template from the html that matched
+// 				if (getHash) { //if there is a hash, put the section in the html
+// 					main.innerHTML = getHash.innerHTML; 
+// 				} else {
+// 					main.innerHTML = document.querySelector('#error').innerHTML; //give the error page
+// 				}
+// 			} else {
+// 				window.location.hash = '#home'; // if there is no hash, give the home page
+// 			}
+// 		}
+// 	};
+
+// 	var hello = {
+// 		greeting: "Ph'nglui mglw'nafh Cthulhu R'lyeh wgah'nagl fhtagn",
+// 		translation: "In his house at R'lyeh, dead Cthulhu waits dreaming."
+// 	};
+
+// 	Transparency.render(document.getElementById('template'), hello);
+
+
+// 	app.init(); //run the app
+
+// }());
 
 reqwest({
     url: 'https://www.rijksmuseum.nl/api/pages/nl/ontdek-de-collectie/overzicht/rembrandt-harmensz-van-rijn?key=jB5D6SNV&format=jsonp', 
