30ee21d89499f68ef528cdf121304b5b8df48580
Linda <lindavandijkdesign@gmail.com>
2016-03-17 12:43:02 +0100

    event.preventDefault()

--- browserTechnologies/week2/fundaCoreFunc/javascript/filter.js
+++ browserTechnologies/week2/fundaCoreFunc/javascript/filter.js
@@ -6,9 +6,10 @@ var filter = (function() {
 			
 			document.querySelector(button).onclick = function() { //Execute this function onclick
 				document.querySelector(button).classList.toggle('background') //change the background
-				document.querySelector(input).classList.toggle('visible') //show input field
+				document.querySelector(input).classList.toggle('invisible') //show input field
 				document.querySelector(img).classList.toggle('turn') //toggle the plus and the cross
 				document.querySelector(button).classList.toggle('bold') //toggle the font bold size
+				event.preventDefault()
 			}
 		}
 	}
