15b684913f3cb9e067270d710d10850cd69ffb78
Linda <lindavandijkdesign@gmail.com>
2016-03-30 20:37:07 +0200

    issue #12 en #15

--- browserTechnologies/week3/script/script.js
+++ browserTechnologies/week3/script/script.js
@@ -1,112 +1,52 @@
-var buttons = document.getElementById('buttonBox').classList.remove('invisible');
-var audioControls = document.getElementById('audioControls').classList.add('invisible');
+// Credits to Sem
 
-var crash = document.getElementById('crashButton');
-var clap = document.getElementById('clapButton');
-var hihat = document.getElementById('hihatButton');
-var snare = document.getElementById('snareButton');
-var ride = document.getElementById('rideButton');
+(function () {
+    'use strict';
+    
+    var audios = document.querySelectorAll('audio');
+    var divs = document.querySelectorAll('div');
+    var handler;
+    var keys = {}
+    
+    function play(evt, button) {
+        
+        if(this) button = this;
+        
+        var audio = button.parentNode.querySelector('audio');
 
-var crashAudio = new Audio('beat/crash.mp3');
-var clapAudio = new Audio('beat/clap.mp3');
-var hihatAudio = new Audio('beat/hihat.mp3');
-var snareAudio = new Audio('beat/snare.mp3');
-var rideAudio = new Audio('beat/ride.mp3');
+        if(audio.paused) {
+            
+            audio.play();
+            audio.currentTime = 0;
+            button.classList.add('glow');
+            button.classList.add('press');
+            return;
+        } 
+        
+        audio.pause();
+        button.classList.remove('glow');
+        button.classList.remove('press');
+    };
+    
+    window.addEventListener('keydown', function(evt) {
+        if(keys[evt.keyCode]) play(null, keys[evt.keyCode]);
+    });
+    
+    if ('ontouchstart' in document.documentElement) handler = 'touchstart';
+    else handler = 'click';
+    
+    for (var i = 0; i < divs.length; i += 1) {
+        var dataKey = divs[i].querySelector('audio').getAttribute('data-key');
+        var keyString = String.fromCharCode(dataKey);
+        var button = document.createElement('button');
+        button.innerHTML = divs[i].querySelector('p').innerHTML + ' (' + keyString + ')';
+        divs[i].appendChild(button);
+        button.addEventListener(handler, play, false);
+        audios[i].removeAttribute('controls');
+        keys[dataKey] = button;
 
+    }
 
-window.onkeydown = function(event) {
-	press = event.keyCode;
-	switch (press) {
-	   	case (65): //a
-	       	crashFunction();
-	    	break;
-		
-		case (83): //s
-		   	clapFunction();
-		   	break;
-
-		case (68): //d
-		   	hihatFunction();
-		   	break;
-
-		case (70): //f
-		  	snareFunction();
-		   	break;
-
-		case (71): //g
-		   	rideFunction();
-		   	break;
-	}
-};
-
-var crashFunction = function() {
-	crashAudio.play();
-	crashAudio.currentTime = 0;
-	crash.classList.add('glow');
-};
-
-var clapFunction = function() {
-	clapAudio.play();
-	clapAudio.currentTime = 0;
-	clap.classList.add('glow');
-}
-
-var hihatFunction = function() {
-	hihatAudio.play();
-	hihatAudio.currentTime = 0;
-	hihat.classList.add('glow');
-}
-
-var snareFunction = function() {
-	snareAudio.play();
-	snareAudio.currentTime = 0;
-	snare.classList.add('glow');
-}
-
-var rideFunction = function() {
-	rideAudio.play();
-	rideAudio.currentTime = 0;
-	ride.classList.add('glow');
-}
-
-crash.addEventListener('animationend', function() {
-	this.classList.remove('glow')
-});
-
-clap.addEventListener('animationend', function() {
-	this.classList.remove('glow')
-});
-
-hihat.addEventListener('animationend', function() {
-	this.classList.remove('glow')
-});
-
-snare.addEventListener('animationend', function() {
-	this.classList.remove('glow')
-});
-
-ride.addEventListener('animationend', function() {
-	this.classList.remove('glow')
-});
-
-
-crash.addEventListener('click', function() {
-	crashFunction();
-});
-
-clap.addEventListener('click', function() {
-	clapFunction();
-});
-
-hihat.addEventListener('click', function() {
-	hihatFunction();
-});
-
-snare.addEventListener('click', function() {
-	snareFunction();
-});
-
-ride.addEventListener('click', function() {
-	rideFunction();
-});
-
+    document.body.classList.add('js');
+    
+}());
