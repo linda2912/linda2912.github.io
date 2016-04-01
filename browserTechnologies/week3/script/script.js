0c1be2d77f0e522d5eb76230ec12eed18e82a876
Linda <lindavandijkdesign@gmail.com>
2016-04-01 11:53:31 +0200

    audio and classless support test

--- browserTechnologies/week3/script/script.js
+++ browserTechnologies/week3/script/script.js
@@ -8,41 +8,53 @@
     var handler = 'click';
     var keys = {}
     
-    window.addEventListener('keydown', function(evt) {
-        if(keys[evt.keyCode]) play(null, keys[evt.keyCode]);
-    });
-    
-    for (var i = 0; i < div.length; i += 1) {
+    var classListAvailable = document.querySelector('body').classList;
 
-        document.body.classList.add('js'); //by adding this class the p element will change to display:block
-        var dataKey = div[i].querySelector('audio').getAttribute('data-key'); //pick all data-key's  
-        var keyString = String.fromCharCode(dataKey); //converts Unicode values into characters.
-        var button = document.createElement('button'); //create button element
-        button.innerHTML = div[i].querySelector('p').innerHTML + ' (' + keyString + ')'; //add html into the button
-        div[i].appendChild(button); //append an button element into all divs
-        button.addEventListener(handler, play, false); //onclick exescute the play function
-        audios[i].removeAttribute('controls'); //remove all the controls from the audio elements  
-        keys[dataKey] = button; 
+    if (classListAvailable) {
+
+        window.addEventListener('keydown', function(evt) {
+            if(keys[evt.keyCode]) play(null, keys[evt.keyCode]);
+        });
+        
+        for (var i = 0; i < div.length; i += 1) {
+
+            document.body.classList.add('js'); //by adding this class the p element will change to display:block
+            var dataKey = div[i].querySelector('audio').getAttribute('data-key'); //pick all data-key's  
+            var keyString = String.fromCharCode(dataKey); //converts Unicode values into characters.
+            var button = document.createElement('button'); //create button element
+            button.innerHTML = div[i].querySelector('p').innerHTML + ' (' + keyString + ')'; //add html into the button
+            div[i].appendChild(button); //append an button element into all divs
+            button.addEventListener(handler, play, false); //onclick exescute the play function
+            audios[i].removeAttribute('controls'); //remove all the controls from the audio elements  
+            keys[dataKey] = button; 
+        }
+
+        function play(evt, button) {
+            
+            if(this) button = this; 
+            
+            var audio = button.parentNode.querySelector('audio'); 
+            var style = button.classList;
+
+            if(audio.paused) {
+                
+                audio.play();
+                audio.currentTime = 0;
+                style.add('glow');
+                style.add('press');
+                return;
+            } 
+            audio.pause();
+            style.remove('glow');
+            style.remove('press');
+        };
     }
 
-    function play(evt, button) {
-        
-        if(this) button = this; 
-        
-        var audio = button.parentNode.querySelector('audio'); 
-        var style = button.classList;
+    var test_audio= document.createElement("audio") //try and create sample audio element 
+    // var test_video= document.createElement("video") //try and create sample video element
+    var mediasupport={audio: (test_audio.play)? true : false}
 
-        if(audio.paused) {
-            
-            audio.play();
-            audio.currentTime = 0;
-            style.add('glow');
-            style.add('press');
-            return;
-        } 
-        audio.pause();
-        style.remove('glow');
-        style.remove('press');
-    };
+    alert("Audio Element support: " + mediasupport.audio)
+
 
 }());
