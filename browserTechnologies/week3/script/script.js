bbfad4f8d35e3ce2bbed00f7ad227b185c970885
Linda <lindavandijkdesign@gmail.com>
2016-04-01 17:37:41 +0200

    audio classlist support

--- browserTechnologies/week3/script/script.js
+++ browserTechnologies/week3/script/script.js
@@ -1,4 +1,4 @@
-// Credits to Sem
+
 
 (function () {
     'use strict';
@@ -15,22 +15,22 @@
         var mediaSupport = true;
     } 
 
-    if (classListAvailable && mediaSupport) {
+    if (classListAvailable && mediaSupport) { //check if classlist and audio is supported
 
-        window.addEventListener('keydown', function(evt) {
+        window.addEventListener('keydown', function(evt) { //credits to Sem for the code below
             if(keys[evt.keyCode]) play(null, keys[evt.keyCode]);
         });
         
-        for (var i = 0; i < div.length; i += 1) {
+        for (var i = 0; i < div.length; i += 1) { 
 
             document.body.classList.add('js'); //by adding this class the p element will change to display:block
             var dataKey = div[i].querySelector('audio').getAttribute('data-key'); //pick all data-key's  
-            var keyString = String.fromCharCode(dataKey); //converts Unicode values into characters.
+            var keyString = String.fromCharCode(dataKey); //converts unicode values into characters.
             var button = document.createElement('button'); //create button element
             button.innerHTML = div[i].querySelector('p').innerHTML + ' (' + keyString + ')'; //add html into the button
             div[i].appendChild(button); //append an button element into all divs
-            button.addEventListener(handler, play, false); //onclick exescute the play function
             audios[i].removeAttribute('controls'); //remove all the controls from the audio elements  
+            button.addEventListener(handler, play, false); //onclick exescute the play function
             keys[dataKey] = button; 
         }
 
