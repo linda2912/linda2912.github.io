49c8e5d46d2255d413c9ca3b9a3e9c1618af4d52
Linda <lindavandijkdesign@gmail.com>
2016-04-12 21:45:02 +0200

    soccer app

---
+++ realTimeWeb/week2/README.md
@@ -0,0 +1,24 @@
+##Real Time Web ~ week 2
+
+####Structuur files
+
+
+```
+soccer-app  
+		└── both
+				├── collections
+						└── posts.js
+				└──	 router.js
+		└── client
+				├── main
+						├── main.css
+						├── main.html
+						└── main.js
+				└── post
+						└── post.html
+				└── posts
+						├── posts.html
+						└── posts.js
+		└── server
+				└── main.js
+```
--- realTimeWeb/week2/soccer-app/both/collections/posts.js
+++ realTimeWeb/week2/soccer-app/both/collections/posts.js
@@ -1,21 +1,26 @@
 Posts = new Mongo.Collection("posts");
 Posts.attachSchema(new SimpleSchema({
-  place: {
-    type: String,
-    label: "Uit of thuis",
-    allowedValues: ['Uit', 'Thuis'],
-    	autoform: {
-    		options: [
-    		{label: "Uit", value: "Uit"},
-    		{label: "Thuis", value: "Thuis"}]
-    	}
-  },
-  against: {
-    type: String,
-    label: "Tegenstander"
-  },
-  date: {
-    type: Date,
-    label: "Datum"
-  }
+    place: {
+        type: String,
+        label: "Uit of thuis",
+        allowedValues: ['Uit', 'Thuis'],
+        	autoform: {
+        		options: [
+        		{label: "Uit", value: "Uit"},
+        		{label: "Thuis", value: "Thuis"}]
+        	}
+    },
+    against: {
+        type: String,
+        label: "Tegenstander"
+    },
+    datetime: {
+        type: String,
+        label: "Datum/ tijd",
+        autoform: {
+            afFieldInput: {
+                type: "datetime-local"
+            }
+        }
+    }
 }));
